362e568524eb458149e43acec620f6cf83b6865e
Minjer <minjer@foxmail.com>
2023-11-03 20:32:18 +0800

    完成了和前端互联的基本框架，等待后续添加具体内容

--- BackEnd/src/main/java/com/minjer/controller/CommentController.java
+++ BackEnd/src/main/java/com/minjer/controller/CommentController.java
@@ -1,2 +1,21 @@
-package com.minjer.controller;public class CommentController {
+package com.minjer.controller;
+
+import com.minjer.pojo.Result;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommentController {
+    /**
+     * 感知由用户上传的评论信息
+     * 未开始
+     * @return
+     */
+    @RequestMapping(value = "/api/v1/comments",method = RequestMethod.POST)
+    public Result addComments(){
+        return Result.success("感知由用户上传的评论信息");
+    }
+
+
 }
--- BackEnd/src/main/java/com/minjer/controller/SentenceController.java
+++ BackEnd/src/main/java/com/minjer/controller/SentenceController.java
@@ -1,2 +1,23 @@
-package com.minjer.controller;public class SentenceController {
+package com.minjer.controller;
+
+import com.minjer.pojo.Result;
+import com.minjer.pojo.Sentence;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+public class SentenceController {
+    /**
+     * 快速感知评论信息
+     * 未开始
+     * @param sentences
+     * @return
+     */
+    @RequestMapping(value = "/api/v1/sentence", method = RequestMethod.POST)
+    public Result handleSentences(ArrayList<Sentence> sentences) {
+        return Result.success("快速感知评论信息");
+    }
 }
